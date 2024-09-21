bd2434b3ac96eefa5826f866fd5a33467c588604
091700 <1020040917@qq.com>
2024-09-21 21:06:43 +0800

    Update 2.java

--- Second/2.java
+++ Second/2.java
@@ -2,11 +2,11 @@ import java.until.Scanner;
 public class zy2{
 public static void main(String[] args){
 Scanner input = new Scanner (System.in);
+System.out.println("Enter the subtotal and a gratuity rate:");
 double sub=input.nextDouble();
 double rate=input.nextDouble();
 double gratuity=sub*rate;
 double total=sub+gratuity;
-System.out.println("Enter the subtotal and a gratuity rate:");
- System.out.println("The gratuity is &"+gratuity+"and total is $"+total);
+System.out.println("The gratuity is &"+gratuity+"and total is $"+total);
 }
 }
