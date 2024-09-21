76d7f29ce623cae003781804806a981e14046b90
091700 <1020040917@qq.com>
2024-09-21 18:42:52 +0800

    Update 9.java

--- First/9.java
+++ First/9.java
@@ -1,5 +1,11 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, World");
-	}
+public class zy9 {
+    public static void main(String[] args) {
+        double km = 14.0;
+        double totalTimeInMinutes = 45.0 + 30.0 / 60.0;
+        double timeInHours = totalTimeInMinutes / 60.0;
+        double kmToMiles = 1.6;
+        double miles = km/ kmToMiles;
+        double averageSpeed = miles / timeInHours;
+        System.out.println("Average speed:"+averageSpeed+" miles per hour");
+    }
 }
