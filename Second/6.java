ebc064cb81983956471ed14c166eaef8ea6d3453
091700 <1020040917@qq.com>
2024-09-21 21:24:33 +0800

    Update 6.java

--- Second/6.java
+++ Second/6.java
@@ -1,9 +1,15 @@
-public class zy6 {
-    public static void main(String[] args) {
-        int sum=0;
-        for (int i = 0; i < 10; i++) {
-            sum+=i;   
+import java.util.Scanner;
+public class zy6{
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+        double monthlyInterestRate = 0.05 / 12;
+        System.out.print("Enter the monthly saving amount: ");
+        double monthlySavingAmount = scanner.nextDouble();
+        double accountValue = monthlySavingAmount;
+        for (int month = 2; month <= 6; month++) {
+            accountValue += accountValue*monthlyInterestRate;
+            accountValue += monthlySavingAmount;
         }
-        System.out.println(sum);
+        System.out.printf("After the sixth month,the account value is "+accountValue);
     }
 }
