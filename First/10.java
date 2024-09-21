a73b061848d3fd9b1aff13c2577cbdc47baa81eb
091700 <1020040917@qq.com>
2024-09-21 18:41:21 +0800

    Update 10.java

--- First/10.java
+++ First/10.java
@@ -1,1 +1,11 @@
-
+public class z1_10 {
+    public static void main(String[] args) {
+        double w= 4.5;
+        double h= 7.9;
+        double area = w* h;
+        double perimeter = 2*(w+h);
+    
+        System.out.println("Area of the rectangle: " +area);
+        System.out.println("Perimeter of the rectangle: " +perimeter);
+    }
+}
