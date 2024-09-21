00aa28d54a511253c2b3b12a195991667d05ba31
091700 <1020040917@qq.com>
2024-09-21 21:25:55 +0800

    Update 3.java

--- Second/3.java
+++ Second/3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class BMICalculator {
+public class zy3{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter weight in pounds: ");
