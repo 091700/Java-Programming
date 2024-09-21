2302b97e3d9745227da8676635ed88b260fe5529
091700 <1020040917@qq.com>
2024-09-21 18:53:46 +0800

    Create 6.java

---
+++ Second/6.java
@@ -0,0 +1,9 @@
+public class zy6 {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i = 0; i < 10; i++) {
+            sum+=i;   
+        }
+        System.out.println(sum);
+    }
+}
