7492a5fb10ddfb45d18aa56545c7d5dd98f3dc5f
091700 <1020040917@qq.com>
2024-09-21 18:21:49 +0800

    Create 13.java

---
+++ First/13.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String[] args) {
+    double a=3.4;
+    double b=50.2;
+    double c=2.1;
+    double d=0.55;
+    double e=44.5;
+    doouble f=5.9;
+    double g=a*d-b*c;
+    if(d!=o)
+    {
+      double x=(e*d-b*f)/g;
+      double y=(a*f-e*c)/g;
+System.out.println("x ="+x); 
+System.out.println("y ="+y);
+    }
+    else{
+		System.out.println("no da an");
+	}
+  }
+}
