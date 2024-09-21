4dd11dfc1c8472b4ee40a76a3d394ff8aa981a16
091700 <1020040917@qq.com>
2024-09-21 20:53:24 +0800

    Update 2.java

--- Second/2.java
+++ Second/2.java
@@ -3,12 +3,11 @@ public class zy2{
 public static void main(String[] args){
 Scanner input1 = new Scanner (System.in);
 Scanner input2 = new Scanner (System.in);
-System.out.println(“");
-double radius=input.nextDouble();
-double area=radius*radius*PI;
-double volume=4/3*PI*radius*radiusradius;
-System.out.println("Enter the radius and length of a cylinder:");
-  System.out.println("The area is "+area);
-System.out.println("The volume is "+volume);
+double sub=input1.nextDouble();
+double rate=input2.nextDouble();
+double gratuity=sub*rate;
+double total=sub+gratuity;
+System.out.println("Enter the subtotal and a gratuity rate:");
+ System.out.println("The gratuity is &"+gratuity+"and total is $"+total);
 }
 }
