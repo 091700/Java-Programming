68fa33466e3af59852aa7b948bf2037e078317e2
091700 <1020040917@qq.com>
2024-09-21 20:59:33 +0800

    Update 3.java

--- Second/3.java
+++ Second/3.java
@@ -1,9 +1,14 @@
-public class zy3{
+import java.util.Scanner;
+public class BMICalculator {
     public static void main(String[] args) {
-        int sum=0;
-        for (int i = 0; i < 10; i++) {
-            sum+=i;   
-        }
-        System.out.println(sum);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        double weightInPounds = scanner.nextDouble();
+        System.out.print("Enter height in inches: ");
+        double heightInInches = scanner.nextDouble();
+        double weightInKilograms = weightInPounds * 0.45359237;
+        double heightInMeters = heightInInches * 0.0254;
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+        System.out.printf("BMI is: "+bmi);
     }
 }
