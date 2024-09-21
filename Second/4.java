0761c3b00845ea79c40555d5c82298e154689d3c
091700 <1020040917@qq.com>
2024-09-21 21:04:32 +0800

    Update 4.java

--- Second/4.java
+++ Second/4.java
@@ -1,9 +1,15 @@
-public class zy4{
+import java.util.Scanner;
+public class DrivingCostCalculator {
     public static void main(String[] args) {
-        int sum=0;
-        for (int i = 0; i < 10; i++) {
-            sum+=i;   
-        }
-        System.out.println(sum);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the driving distance: ");
+        double distance = scanner.nextDouble();
+        System.out.print("Enter the miles per gallon: ");
+        double fuelEfficiency = scanner.nextDouble();
+        System.out.print("Enter the price per gallon: ");
+        double pricePerGallon = scanner.nextDouble();
+        double gallonsNeeded = distance / fuelEfficiency;
+        double cost= gallonsNeeded * pricePerGallon;
+        System.out.printf("The cost of the trip is: $", cost);
     }
 }
