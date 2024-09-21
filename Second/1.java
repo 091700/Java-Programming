a5c12d9d32803d5a74b18733a18784a711464d3b
091700 <1020040917@qq.com>
2024-09-21 21:05:59 +0800

    Update 1.java

--- Second/1.java
+++ Second/1.java
@@ -2,12 +2,12 @@ import java.until.Scanner;
 public class Main{
 public static void main(String[] args){
 final double PI=3.1415926;
+Scanner input = new Scanner (System.in);
 System.out.println("Enter the radius and length of a cylinder:");
-  Scanner input = new Scanner (System.in);
 double radius=input.nextDouble();
 double area=radius*radius*PI;
 double volume=4/3*PI*radius*radiusradius;
-  System.out.println("The area is "+area);
+System.out.println("The area is "+area);
 System.out.println("The volume is "+volume);
 }
 }
