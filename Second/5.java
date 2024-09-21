d60a2ea8a5afbc4bf09bd5c15ec52697612b51c9
091700 <1020040917@qq.com>
2024-09-21 21:13:22 +0800

    Update 5.java

--- Second/5.java
+++ Second/5.java
@@ -1,9 +1,10 @@
-public class zy5{
+import java.util.Scanner;
+public class HexagonAreaCalculator {
     public static void main(String[] args) {
-        int sum=0;
-        for (int i = 0; i < 10; i++) {
-            sum+=i;   
-        }
-        System.out.println(sum);
-    }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the length of the side: ");
+    double sideLength = scanner.nextDouble();
+    double area = (3 * Math.sqrt(3) / 2) * Math.pow(sideLength, 2);
+    System.out.println("The area of the hexagon is ", area);
+}
 }
