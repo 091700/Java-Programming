2fd04f98ef92a0e67768eaa627a2cfa9477b7294
091700 <1020040917@qq.com>
2024-09-21 18:44:17 +0800

    Update 8.java

--- First/8.java
+++ First/8.java
@@ -1,1 +1,10 @@
-
+public class zy8 {
+    public static void main(String[] args) {
+        double radius = 5.5;
+        double pi = 3.14159; 
+        double perimeter = 2 * radius * pi;
+        double area = radius * radius * pi;
+        System.out.println("Perimeter of the circle: " + perimeter);
+        System.out.println("Area of the circle: " + area);
+    }
+}
