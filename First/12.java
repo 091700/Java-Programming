6d5c90753e43320557035b130d15a4c15620405a
091700 <1020040917@qq.com>
2024-09-21 18:27:46 +0800

    Update 12.java

--- First/12.java
+++ First/12.java
@@ -1,1 +1,11 @@
-
+public class zy12 {
+    public static void main(String[] args) {
+        double miles = 24.0;
+        double totalTimeInMinutes = 60+40+35.0/60.0;
+        double timeInHours = totalTimeInMinutes / 60.0;
+        double m= 1.6;
+        double k= miles * m;
+        double averageSpeed = k/timeInHours;
+        System.out.println("Average speed: " + averageSpeed + " kilometers per hour");
+    }
+}
