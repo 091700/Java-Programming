2c03f0eec8b87eaa36854453a148c2a13ac8c823
091700 <1020040917@qq.com>
2024-09-21 18:46:13 +0800

    Update 7.java

--- First/7.java
+++ First/7.java
@@ -1,1 +1,9 @@
-
+public class z7 {
+    public static void main(String[] args) {
+        double a1 = 1.0;
+        double p1 = 4 * (a1 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11);
+        double p2 = 4 * (a1 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
+        System.out.println("First approximation of p: " + p1);
+        System.out.println("Second approximation of p: " + p2);
+    }
+}
