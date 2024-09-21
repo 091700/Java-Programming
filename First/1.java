c69d122e393b8776f0cce81ff9011dccbb92f56f
091700 <1020040917@qq.com>
2024-09-21 18:07:03 +0800

    Update 1.java

--- First/1.java
+++ First/1.java
@@ -1,6 +1,7 @@
 pulic class q1{
     public static void main(String[ args]){
-        System.out.println("Welcome to Java welcome to Computer Science");
+        System.out.println("Welcome to Java ");
+         System.out.println("welcome to Computer Science");
         System.out.println("Programing is fun");
     }
     }
