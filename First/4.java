a818529f4405159b4b07446efaaa77a1ad968d13
091700 <1020040917@qq.com>
2024-09-21 18:10:50 +0800

    Update 4.java

--- First/4.java
+++ First/4.java
@@ -1,9 +1,6 @@
-public class WelcomeToJava {
+public class 4 {
     public static void main(String[] args) {
-            System.out.println("a   a^2 a^3");
-            System.out.println("1   1   1");
-            System.out.println("2   4   8");       
-            System.out.println("3   9   27"); 
-            System.out.println("4   16  64"); 
+        fpr(int 1=0;i<4;i++)
+            System.out.println(i+"   "+i*i+"   "+i*i*i);
     }
 }
