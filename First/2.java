954990972a1ec3610be2c104ebcca39b311fe86d
091700 <1020040917@qq.com>
2024-09-21 18:11:23 +0800

    Update 2.java

--- First/2.java
+++ First/2.java
@@ -1,4 +1,4 @@
-public class WelcomeToJava {
+public class 2 {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("Welcomr to Java");
