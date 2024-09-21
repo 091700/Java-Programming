5d622f50ed0f37288b82e6ad62b80b15e7021b04
091700 <1020040917@qq.com>
2024-09-21 20:53:58 +0800

    Update 1.java

--- Second/1.java
+++ Second/1.java
@@ -3,7 +3,6 @@ public class Main{
 public static void main(String[] args){
 final double PI=3.1415926;
 Scanner input = new Scanner (System.in);
-System.out.println(“Enter a number for radius:");
 double radius=input.nextDouble();
 double area=radius*radius*PI;
 double volume=4/3*PI*radius*radiusradius;
