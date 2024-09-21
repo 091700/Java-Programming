297aeeec54b0e41a04558c650ff10258df9d495e
091700 <1020040917@qq.com>
2024-09-21 18:48:50 +0800

    Update 6.java

--- First/6.java
+++ First/6.java
@@ -1,1 +1,10 @@
-
+public class zy6 {
+    public static void main(String[] args) {
+        int a=0;
+        for (int i = 0; i < 9; i++) 
+        {
+            a+=i;   
+        }
+        System.out.println(a);
+    }
+}
