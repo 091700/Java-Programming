e9b0e58716434f5aae6bddd198ea9a71db8477b5
091700 <1020040917@qq.com>
2024-09-21 19:05:38 +0800

    Update 1.java

--- Second/1.java
+++ Second/1.java
@@ -4,8 +4,11 @@ public static void main(String[] args){
 final double PI=3.1415926;
 Scanner input = new Scanner (System.in);
 System.out.println(Enter a number for radius:");
-double radius=input.nextDoublr();
+double radius=input.nextDouble();
 double area=radius*radius*PI;
-System.out.println("The area for the circle of radius"+radius+"is"+area);
+double volume=4/3*PI*radius*radiusradius;
+System.out.println("Enter the radius and length of a cylinder:");
+  System.out.println("The area is "+area);
+System.out.println("The volume is "+volume);
 }
 }
