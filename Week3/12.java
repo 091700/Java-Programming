5311d6b65d464f7482455c029e48becbb2389f80
091700 <1020040917@qq.com>
2024-09-30 11:18:53 +0800

    Create 12.java

---
+++ Week3/12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PayIncrement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your pay: ");
+        double pay = input.nextDouble();
+        System.out.println("Your pay without increment is: " + pay);
+        System.out.print("Enter your score: ");
+        double score = input.nextDouble();
+        if (score > 90) {
+            double new_pay = pay + (pay * 0.03);
+            System.out.println("Your pay with increment is: " + new_pay);
+        }
+    }
+}
