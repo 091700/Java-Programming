ab0d48f43468476a7859dfad6b4487335cbc85d9
091700 <1020040917@qq.com>
2024-09-30 10:50:27 +0800

    Create 4.java

---
+++ Week3/4.java
@@ -0,0 +1,8 @@
+public class ExponentOperation {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.pow(4, 0.5));
+        System.out.println(Math.pow(2.5, 2));
+        System.out.println(Math.pow(2.5, -2));
+    }
+}
