46673e2000ca44288070a41328f505cac09ad237
091700 <1020040917@qq.com>
2024-09-30 09:15:11 +0800

    Create 2.java

---
+++ Week4/2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        switch (year % 12) {
+            case 0:
+                System.out.println("monkey");
+                break;
+            case 1:
+                System.out.println("rooster"); // 注意：这里应为"rooster"的正确拼写是"rooster"（虽然可能是拼写错误，但按照图片内容提取）
+                break;
+            case 2:
+                System.out.println("dog");
+                break;
+            case 3:
+                System.out.println("pig");
+                break;
+            case 4:
+                System.out.println("rat");
+                break;
+            case 5:
+                System.out.println("ox");
+                break;
+            case 6:
+                System.out.println("tiger");
+                break;
+            case 7:
+                System.out.println("rabbit");
+                break;
+            case 8:
+                System.out.println("dragon");
+                break;
+            case 9:
+                System.out.println("snake");
+                break;
+            case 10:
+                System.out.println("horse");
+                break;
+            case 11:
+                System.out.println("sheep");
+                break;
+        }
+    }
+}
