de091547d17b6b181d7cc20c871a5b2c7beee215
091700 <1020040917@qq.com>
2024-10-12 09:17:27 +0800

    Create 7.java

---
+++ Week4/7.java
@@ -0,0 +1,14 @@
+//java 1
+import java.until.scanner;
+public class Main {
+public  static void main(String[] args)
+Scanner input =new Scanner(System.in);
+System.out.println("enter the radius and length of a cylinder:");
+double radius=input.nextDouble;
+double length=input.nextDouble;
+double area=radius*radius*3.14;
+double volume=area*length;
+System.out.println("The area is "+area);
+System.out.println("The volume is "+volume);
+    
+}
