54b3adb1e44ad56b9199a9b9db0469c7b904b75f
091700 <1020040917@qq.com>
2024-10-12 18:16:26 +0800

    Create 12.java

---
+++ Week5/12.java
@@ -0,0 +1,9 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        System.out.println(Math.max(2, 3));
+        System.out.println(Math.min(2.5, 4.6)); 
+        System.out.println(Math.max(Math.max(2.5, 4.6), Math.min(3, 5.6))); 
+        System.out.println(Math.abs(-2));
+        System.out.println(Math.abs(-2.1)); 
+    }
+}
