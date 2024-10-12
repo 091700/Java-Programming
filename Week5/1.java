8dacb8dd9c1c2d98f44b0f42beaeab5c50181360
091700 <1020040917@qq.com>
2024-10-12 09:54:50 +0800

    Update 1.java

--- Week5/1.java
+++ Week5/1.java
@@ -1,1 +1,10 @@
-
+public class TrigonometricFunctions {
+    public static void main(String[] args) {
+        System.out.println("The value of sin(PI/2): " + Math.sin(Math.PI / 2));
+        System.out.println("The value of cos(PI/2): " + Math.cos(Math.PI / 2));
+        System.out.println("The value of tan(PI): " + Math.tan(0)); 
+        System.out.println("The value of asin(0.5): " + Math.asin(0.5));
+        System.out.println("The value of acos(0.5): " + Math.acos(0.5));
+        System.out.println("The value of atan(1): " + Math.atan(1));
+    }
+}
