135dec1bde68a80b4764c1ab01c4350ef38c2d4c
091700 <1020040917@qq.com>
2024-10-12 09:56:28 +0800

    Create 5.java

---
+++ Week5/5.java
@@ -0,0 +1,10 @@
+public class ExponentMethods {
+    public static void main(String[] args) {
+        System.out.println("The value of exp(3.5): " + Math.exp(3.5));
+        System.out.println("The value of log(3.5): " + Math.log(3.5));
+        System.out.println("The value of log10(3.5): " + Math.log10(3.5));
+        System.out.println("The value of pow(3, 2): " + Math.pow(3, 2));
+        System.out.println("The value of Sqrt(4): " + Math.sqrt(4));
+        System.out.println("The value of Sqrt(10.5): " + Math.sqrt(10.5));
+    }
+}
