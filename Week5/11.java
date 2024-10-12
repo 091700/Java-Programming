da9ba25bbdfce41bdf591ad013df8a6091415388
091700 <1020040917@qq.com>
2024-10-12 16:54:25 +0800

    Update 11.java

--- Week5/11.java
+++ Week5/11.java
@@ -1,10 +1,1 @@
-public class CheckCharMethods {
-    public static void main(String[] args) {
-        System.out.println("isDigit('a') is " + Character.isDigit('a'));
-        System.out.println("isLetter('a') is " + Character.isLetter('a'));
-        System.out.println("isLowerCase('a') is " + Character.isLowerCase('a'));
-        System.out.println("isUpperCase('a') is " + Character.isUpperCase('a'));
-        System.out.println("isLowerCase('T') is " + Character.isLowerCase('T'));
-        System.out.println("isUpperCase('q') is " + Character.isUpperCase('q'));
-    }
-}
+
