31fea8ad0cf2f61ffaa1f6beb6c49d63a1a5b39c
091700 <1020040917@qq.com>
2024-10-12 09:20:53 +0800

    Create 8.java

---
+++ Week4/8.java
@@ -0,0 +1,14 @@
+//java 2
+import java.until.Scanner;
+public class Main {
+public  static void main(String[] args)
+Scanner input =new Scanner(System.in);
+System.out.println("enter the subtotal and a gratuity rate:");
+double subtotal=input.nextDouble;
+double gratuityrate=input.nextDouble;
+double gratuity=subtotal*gratuityrate;
+double total=subtotal+gratuity;
+System.out.print("The gratuity is"+gratuity);
+System.out.println("and total is is "+total);
+    
+}
