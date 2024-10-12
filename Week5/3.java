e0072160a30c0527c57ab4711c8308e20a8dd0e4
091700 <1020040917@qq.com>
2024-10-12 10:05:30 +0800

    Create 3.java

---
+++ Week5/3.java
@@ -0,0 +1,24 @@
+public class MathOperations {
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(2.1));
+        System.out.println(Math.ceil(2.0)); 
+        System.out.println(Math.ceil(-2.0));
+        System.out.println(Math.ceil(-2.1)); 
+        System.out.println(Math.floor(2.1)); 
+        System.out.println(Math.floor(2.0)); 
+        System.out.println(Math.floor(-2.0)); 
+        System.out.println(Math.floor(-2.1)); 
+        System.out.println(Math.round(2.1));
+        System.out.println(Math.round(2.0)); 
+        System.out.println(Math.round(-2.0)); 
+        System.out.println(Math.round(-2.1)); 
+        System.out.println(Math.round(2.5));
+        System.out.println(Math.round(3.5)); 
+        System.out.println(Math.round(-2.5)); 
+        System.out.println(Math.round(2.6f));
+        System.out.println(Math.round(2.0f)); 
+        System.out.println(Math.round(-2.0f)); 
+        System.out.println(Math.round(-2.6f));
+        System.out.println(Math.round(-2.4f)); 
+    }
+}
