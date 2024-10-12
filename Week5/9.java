72a734e58afacc0b6186c9db5f1f7ff74375b518
091700 <1020040917@qq.com>
2024-10-12 11:21:22 +0800

    Create 9.java

---
+++ Week5/9.java
@@ -0,0 +1,12 @@
+
+public static void checkChar{
+  public static void main(String[] args){
+  char ch="8";
+    if (ch >= 'A' && ch <= 'Z') {
+        System.out.println(ch + "uppercase");
+    } else if (ch >= 'a' && ch <= 'z') {
+        System.out.println(ch + "lowercase");
+    } else if (ch >= '0' && ch <= '9') {
+        System.out.println(ch + " number");
+    }
+}}
