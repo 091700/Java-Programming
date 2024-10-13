1fcaddf3a44538a0620bcb63bd3083fc1497c98b
091700 <1020040917@qq.com>
2024-10-13 13:39:27 +0800

    Create 15.java

---
+++ Week5/15.java
@@ -0,0 +1,6 @@
+public class GetCharFromString {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        System.out.println("The first character in message is " + message.charAt(0));
+    }
+}
