799a7a553a83b12dad8949716ac47b2a887d6bed
091700 <1020040917@qq.com>
2024-10-13 13:49:03 +0800

    Create 22.java

---
+++ Week5/22.java
@@ -0,0 +1,8 @@
+public class WeekTask19 {
+    public static void main(String[] args) {
+        int count = 5;
+        double amount = 45.56;
+        // 使用printf格式化输出，%8d表示整数占8位，右对齐，%.10f表示浮点数保留10位小数
+        System.out.printf("%8d,%.10f\n", count, amount);
+    }
+}
