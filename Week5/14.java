43ab26f5ac2f49dc15e1fd21914f18300584516c
091700 <1020040917@qq.com>
2024-10-13 13:39:12 +0800

    Create 14.java

---
+++ Week5/14.java
@@ -0,0 +1,6 @@
+public class StringLengthExample {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        System.out.println("The length of " + message + " is " + message.length());
+    }
+}
