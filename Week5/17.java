d4192da0923d6b392f0c50d98c615631328eadcf
091700 <1020040917@qq.com>
2024-10-13 13:40:03 +0800

    Create 17.java

---
+++ Week5/17.java
@@ -0,0 +1,11 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        String s1 = "Chapter2";
+        String s2 = "s2";
+        String s3 = "s3";
+        System.out.println(message + s1 + s2);
+        System.out.println(s1 + s2);
+        System.out.println(s1 + s3);
+    }
+}
