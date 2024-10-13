5a58490301cba146e98ece772cee5a9e384d83aa
091700 <1020040917@qq.com>
2024-10-13 14:06:43 +0800

    Create 15.java

---
+++ Week4/15.java
@@ -0,0 +1,8 @@
+public class Labtask {    
+  public static void main(String[] args) 
+  {        
+   int num1 = 5;        
+   int num2 = 8;        
+   int max = (num1>num2)?num1:num2;        
+   System.out.println(max);    
+  }}
