f5bb12c1e11d5cc551eb9ffe27a3d10abd3fd051
091700 <1020040917@qq.com>
2024-10-13 13:39:44 +0800

    Create 16.java

---
+++ Week5/16.java
@@ -0,0 +1,7 @@
+public class ConvertingStrings {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        System.out.println("The message in lowercase is: " + message.toLowerCase());
+        System.out.println("The message in UPPERCASE is: " + message.toUpperCase());
+    }
+}
