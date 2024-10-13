6af961aed18cef1e944a1d88be71dff07843e58d
091700 <1020040917@qq.com>
2024-10-13 14:08:07 +0800

    Create 16.java

---
+++ Week4/16.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;public class Labtask17 {    
+  public static void main(String[] args) {        
+  Scanner input = new Scanner(System.in);        
+  System.out.println("Enter a number: ");        
+int number = input.nextInt();        
+String formattedNumber = String.format("%02d", number);       
+System.out.println("Formatted number: " + formattedNumber);    
+}}
