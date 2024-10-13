c7251f602c6387adce518cd151db9e0cd608a8b2
091700 <1020040917@qq.com>
2024-10-13 13:49:24 +0800

    Create 23.java

---
+++ Week5/23.java
@@ -0,0 +1,8 @@
+public class WeekTask20 {
+    public static void main(String[] args) {
+        int count = 8;
+        double amount = 8.81;
+        System.out.printf("%-8d%-8.1f\n", count, amount);
+        System.out.printf("%-8d%-8.1f\n", count, amount);
+    }
+}
