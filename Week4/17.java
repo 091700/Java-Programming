4547d2ece1f706d94320b704f207325c30436f81
091700 <1020040917@qq.com>
2024-10-13 14:09:36 +0800

    Create 17.java

---
+++ Week4/17.java
@@ -0,0 +1,7 @@
+public class Labtask{        
+  public static void main(String[] args) {           
+  double miles = 100;           
+final double kilometers_per_miles = 1.609;            
+double kilometers = miles * kilometers_per_miles;            
+System.out.println(kilometers);        
+}    }
