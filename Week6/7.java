7cfcac9ca4695698e822e7bfadccdf41c3c6b6b1
091700 <1020040917@qq.com>
2024-10-14 11:11:40 +0800

    Create 7.java

---
+++ Week6/7.java
@@ -0,0 +1,17 @@
+//7
+public class main{
+    public void Main(String[] args){
+        for (int i=0;i<10;i++);
+        sum+=1;
+        if(i<j);
+        System.out.println(i);
+        else
+        System.out.println(j);
+        while(j<10){
+            j++
+        }
+        do{
+            j++
+        }while(j<10)
+    }
+}
