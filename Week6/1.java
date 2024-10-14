95ce83d94d33a49b79b60834630ff87446c5840b
091700 <1020040917@qq.com>
2024-10-14 11:07:47 +0800

    Create 1.java

---
+++ Week6/1.java
@@ -0,0 +1,10 @@
+//1
+public class Main{
+    public static void main(String[] args){
+        int sum=0;i=1;
+        while(i<10){
+            sum=sum+i;
+        }
+        system.out.println("sum is"+sum);
+    }
+}
