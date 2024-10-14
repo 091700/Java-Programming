01c6486d899c821b24d5d4ddd5b51f44a81ecd10
091700 <1020040917@qq.com>
2024-10-14 11:12:04 +0800

    Create 8.java

---
+++ Week6/8.java
@@ -0,0 +1,17 @@
+//8
+public class main{
+    public static void Main(String[] aargs){
+        System.out.print("    Multiplication table");
+        System.out.print("    ")
+for(int j=1;j<9;j++)
+System.out.print("    "+j);
+System.out.print("\n------------------------------------");
+for(int i=1;i<9;i++)
+{System.out.print(i+"|");
+for(int j=1;j<9;j++){
+    system.out.printf("%5d",i*j);
+}
+System.out.println();
+}
+    }
+}
