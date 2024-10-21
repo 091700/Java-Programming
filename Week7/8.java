46654bb25eb95a4eda38712b451a464bd0685ff9
091700 <1020040917@qq.com>
2024-10-21 11:25:54 +0800

    Create 8.java

---
+++ Week7/8.java
@@ -0,0 +1,31 @@
+//Task8 prime number
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The first 50 prime numbers are:");
+        printPrimeNumbers(50);
+    }
+    public static void printPrimeNumbers(int numberofPrimes) {
+        final int NUMBER_OF_PRIMES_PER_LINE = 10; 
+        int count = 0;
+        int number = 2; 
+        while (count < numberofPrimes) {
+            if (isPrime(number)) {
+                count++; 
+                if (count % NUMBER_OF_PRIMES_PER_LINE == 0) {
+                    System.out.printf("%-5d\n", number);
+                } else 
+                {
+                    System.out.printf("%-5d", number);}
+            }
+            number++;
+        }
+    }
+    public static boolean isPrime(int number) {
+        for (int divisor = 2; divisor < number / 2; divisor++) { 
+            if (number % divisor == 0) { 
+                return false;
+            }
+        }
+        return true;
+    }
+}
