5d374ee93dd820e0b13b5d892ff5ebadfbbc3cdd
091700 <1020040917@qq.com>
2024-10-21 11:22:45 +0800

    Update 1.java

--- Week7/1.java
+++ Week7/1.java
@@ -1,1 +1,14 @@
-
+//Task1
+public class Main {
+	public static int sum(int n1,int n2) {
+	    int sum=0;
+	    for(int i=n1;i<=n2;i++)
+	    sum+=i;
+	    return sum;
+	}
+	public static void main(String[] args){
+	    System.out.println("Sum from 1 to 10 is"+sum(1,10));
+	    System.out.println("Sum from 20 to 30 is"+sum(20,30));
+	    System.out.println("Sum from 35 to 45 is"+sum(35,45));
+	}
+}
