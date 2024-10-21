ad2fffc654091d76313c24ded37a44341557b86a
091700 <1020040917@qq.com>
2024-10-21 11:24:36 +0800

    Create 5.java

---
+++ Week7/5.java
@@ -0,0 +1,11 @@
+//Task5
+public class Main {
+	public static void main(String[] args) {
+    int x=7;
+    nprint(x);
+	}
+	public static void nprint(int n){
+	    for(int i=0;i<n;i++)
+	    System.out.println("message");
+	}
+}
