f54606b80bf7db0a23eceab166ca48116e42510d
091700 <1020040917@qq.com>
2024-10-21 11:27:09 +0800

    Create 11.java

---
+++ Week7/11.java
@@ -0,0 +1,17 @@
+//Task11
+public class Main{
+    public static void main(String[] args) {
+        final int NUMBER_OF_CHARS = 175;
+        final int CHAR_PER_LINE = 25;
+        for (int i = 0; i < NUMBER_OF_CHARS; i++) {
+            char ch = RandomCharacter.getRandomLowerCaseLetter();
+            if ((i + 1) % CHAR_PER_LINE == 0) {
+                System.out.println(ch);
+            } else {
+                System.out.print(ch);
+            }
+            public static void 
+        }
+        
+    }
+}
