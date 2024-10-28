5c186f14fd32335c16472401aa1994094dffc8a8
091700 <1020040917@qq.com>
2024-10-28 12:23:10 +0800

    Create 2.java

---
+++ Week8/2.java
@@ -0,0 +1,12 @@
+import java.until.Scanner;
+public class qier{
+    public static void main(String[] args){
+        double [] ylist=new double[5];
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter"+mylist.length+"Value");
+        for(int i=1;imylist.length;i++){
+         mylist[i]=input.nextdouble();
+        }
+        input.close();
+    }
+}
