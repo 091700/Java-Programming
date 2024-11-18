3087a12915a4c74bd1780c41ce58ceed0cde0813
091700 <1020040917@qq.com>
2024-11-18 15:04:02 +0800

    Create 1.java

---
+++ Week11/1.java
@@ -0,0 +1,23 @@
+public class arrrays{
+    public static void main(String[] args){
+    int [][] martix=new int[5][4];
+    int [][] martix1={
+        {2,3,4,5},
+        {6,7,8,9},
+        {10,11,12,13}
+    };
+    int [][] array={
+        {1,2,3,4},
+        {4,5,6},
+        {7,8,9},
+        {10,11,12}
+    };
+    System.out.println("THE LENGTH is "+martix.length);
+    System.out.println("THE LENGTH is "+martix1.length);
+    System.out.println("The length is "+array.length);
+    System.out.println("The length1 is "+array[0].length);
+    System.out.println("The length2 is "+array[1].length);
+    System.out.println("The length3 is "+array[2].length);
+    System.out.println("The length4 is "+array[3].length);
+    }
+}
