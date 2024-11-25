80330c74e65dc8f79a7b8eee18f4d3e2559c768a
091700 <1020040917@qq.com>
2024-11-25 12:53:37 +0800

    Create 3.2.java

---
+++ Week12/3.2.java
@@ -0,0 +1,10 @@
+//3
+package package1;
+
+public class Class2 {
+    public static void main(String[] args) {
+        class1 c1 = new class1();
+        System.out.println(c1.x);
+        System.out.println(c1.y); 
+    }
+}
