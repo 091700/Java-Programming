779f79f5b8e6f2df4c561728cdbf81026a743842
091700 <1020040917@qq.com>
2024-11-25 12:52:30 +0800

    Create 2.2.java

---
+++ Week12/2.2.java
@@ -0,0 +1,8 @@
+//2
+public class TestC{
+    public static void main(String[] args){
+       C object1 = new C();
+       System.out.println(object1.x);
+       System.out.println(object1.convert());
+    }
+}
