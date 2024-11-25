0aa2d175f8af0f58f244df0911f58a7ebc9e0588
091700 <1020040917@qq.com>
2024-11-25 12:51:18 +0800

    Create 1.1.java

---
+++ Week12/1.1.java
@@ -0,0 +1,15 @@
+//1
+public class C1{
+    public int x=1;
+    int y=2;
+    private int z =3;
+    public void m1(){
+        System.out.println("I am method 1");
+    }
+    void m2(){
+        System.out.println("I am method 2");
+    }
+    private void m3(){
+        System.out.println("I am method 3");
+    }
+}
